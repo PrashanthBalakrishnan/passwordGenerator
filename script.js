eae06034958c16f1be76af78be620102f32f05fd
unknown <prashanthdilshaan@gmail.com>
2022-07-12 22:13:25 -0400

    added functionality for random pass
    responsive page

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
-let option1 = document.getElementById("option1")
-let option2 = document.getElementById("option2")
-let option3 = document.getElementById("option3")
-let option4 = document.getElementById("option4")
+const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
+    "/"];
 
-let alphaLower = "abcdefghijklmnopqrstuvwxyz".split();
-let alphaUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split();
-let nums = "1234567890".split();
-let sym = " ~`!@#$%^&*()_-+={[}]|\:;\"'<,>.?/".split();
-alphaNumChar = alphaLower + alphaUpper + nums + sym;
+const generateBtn = document.getElementById("generateBtn")
+const feild1 = document.getElementById("password1")
+const feild2 = document.getElementById("password2")
 
-function generatePassword() {
-    firstOption = "";
-    secondOption = "";
-    thirdOption = "";
-    fourthOption = "";
 
-    for (let i = 0; i < 6; i++) {
-        let randomNum = Math.floor(Math.random() * 94)
-        firstOption += alphaNumChar[randomNum];
+
+function getRandomPass() {
+    const password = []
+    for (let i = 0; i < 15; i++) {
+        password.push(characters[Math.floor(Math.random() * characters.length)])
     }
-
-    for (let i = 0; i < 6; i++) {
-        let randomNum = Math.floor(Math.random() * 94)
-        secondOption += alphaNumChar[randomNum];
-    }
-
-    for (let i = 0; i < 6; i++) {
-        let randomNum = Math.floor(Math.random() * 94)
-        thirdOption += alphaNumChar[randomNum];
-    }
-
-    for (let i = 0; i < 6; i++) {
-        let randomNum = Math.floor(Math.random() * 94)
-        fourthOption += alphaNumChar[randomNum];
-    }
-
-    option1.textContent = firstOption;
-    option2.textContent = secondOption;
-    option3.textContent = thirdOption;
-    option4.textContent = fourthOption;
+    return (password.join(""))
 }
+
+function displayFunction() {
+    feild1.innerText = getRandomPass()
+    feild2.innerText = getRandomPass()
+}
+
+generateBtn.addEventListener("click", displayFunction)
+
+
+
+
+
